2ca64eecb57e005108171ad0768bcd7489320a24
FegaMega <david.smidebrant@gmail.com>
2022-11-22 18:24:20 +0100

    Create hr.py

---
+++ hr.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+from pygame.locals import * 
+
+screen = pygame.display.set_mode((800, 800), 0, 32)
+
+while True:
+    screen.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+            pygame.quit()
+
+    pygame.display.update()
